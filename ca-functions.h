11cc8b103847fcd3dc87e1fbb6bfc6bf8ef99c0e
Morten Hustveit <morten.hustveit@gmail.com>
2013-01-28 19:16:46 -0800

    ca-functions.h: Added missing extern "C" for __cplusplus mode
    Signed-off-by: Morten Hustveit <morten.hustveit@gmail.com>

--- ca-functions.h
+++ ca-functions.h
@@ -3,10 +3,18 @@
 
 #include <stddef.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 float
 ca_stats_correlation (const float *lhs, const float *rhs, size_t count);
 
 float
 ca_stats_rank_correlation (const float *values, size_t count);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
 
 #endif /* !CA_FUNCTIONS_H_ */

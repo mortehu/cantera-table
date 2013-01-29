5b349e110829522db3827b672b6079e4d14d30ff
Morten Hustveit <morten.hustveit@gmail.com>
2013-01-29 09:47:16 -0800

    query.c: Enable intersection, subtraction and reversal
    Results are sorted by the score of the first keyword.  If a '~' is prepended,
    the results are returned in reverse order.
    
    When multiple keywords are entered, the intersection of their results is
    returned.  If a '-' is prepended to a keyword, the intersection is performed
    against the complement of that keyword.
    
    Signed-off-by: Morten Hustveit <morten.hustveit@gmail.com>

--- query.c
+++ query.c
@@ -1,6 +1,8 @@
 #include <assert.h>
+#include <string.h>
 
 #include "ca-table.h"
+#include "memory.h"
 
 static size_t
 CA_partition (struct ca_offset_score *data, size_t count, size_t pivot_index)
@@ -51,6 +53,75 @@ ca_quicksort (struct ca_offset_score *data, size_t count)
     }
 }
 
+static size_t
+CA_subtract_offsets (struct ca_offset_score *output,
+                     const struct ca_offset_score *lhs, size_t lhs_count,
+                     const struct ca_offset_score *rhs, size_t rhs_count)
+{
+  struct ca_offset_score *o;
+  const struct ca_offset_score *lhs_end, *rhs_end;
+
+  o = output;
+  lhs_end = lhs + lhs_count;
+  rhs_end = rhs + rhs_count;
+
+  while (lhs != lhs_end && rhs != rhs_end)
+    {
+      if (lhs->offset == rhs->offset)
+        {
+          ++lhs;
+          ++rhs;
+
+          continue;
+        }
+
+      if (lhs->offset < rhs->offset)
+        *o++ = *lhs++;
+      else
+        ++rhs;
+    }
+
+  while (lhs != lhs_end)
+    *o++ = *lhs++;
+
+  return o - output;
+}
+
+static size_t
+CA_intersect_offsets (struct ca_offset_score *output,
+                      const struct ca_offset_score *lhs, size_t lhs_count,
+                      const struct ca_offset_score *rhs, size_t rhs_count)
+{
+  struct ca_offset_score *o;
+  const struct ca_offset_score *lhs_end, *rhs_end;
+
+  o = output;
+  lhs_end = lhs + lhs_count;
+  rhs_end = rhs + rhs_count;
+
+  while (lhs != lhs_end && rhs != rhs_end)
+    {
+      if (lhs->offset == rhs->offset)
+        {
+          o->offset = lhs->offset;
+          o->score = lhs->score;
+          ++o;
+
+          ++lhs;
+          ++rhs;
+
+          continue;
+        }
+
+      if (lhs->offset < rhs->offset)
+        ++lhs;
+      else
+        ++rhs;
+    }
+
+  return o - output;
+}
+
 int
 ca_schema_query (struct ca_schema *schema, const char *query,
                  const char *index_table_name,
@@ -67,12 +138,17 @@ ca_schema_query (struct ca_schema *schema, const char *query,
   const uint8_t *data;
 
   struct ca_offset_score *offsets = NULL;
-  uint32_t i, offset_count;
+  uint32_t i, offset_count = 0;
+
+  char *query_buf = NULL, *token;
 
   ssize_t ret;
   int result = -1;
 
   ca_clear_error ();
+
+  if (!(query_buf = safe_strdup (query)))
+    goto done;
 
   if (!(index_table = ca_schema_table (schema, index_table_name, &index_declaration)))
     goto done;
@@ -122,31 +198,91 @@ ca_schema_query (struct ca_schema *schema, const char *query,
       goto done;
     }
 
-  if (1 != (ret = ca_table_seek_to_key (index_table, query)))
+  for (token = strtok (query_buf, " \t"); token; token = strtok (NULL, " \t"))
     {
-      if (!ret) /* "Not found" is not an error */
-        result = 0;
+      struct ca_offset_score *token_offsets;
+      uint32_t token_offset_count;
+      int invert_rank = 0, subtract = 0;
 
-      goto done;
+      if (*token == '-')
+        ++token, subtract = 1;
+
+      if (*token == '~')
+        ++token, invert_rank = 1;
+
+      if (1 != (ret = ca_table_seek_to_key (index_table, token)))
+        {
+          if (!ret) /* "Not found" is not an error */
+            result = 0;
+
+          goto done;
+        }
+
+      if (1 != (ret = ca_table_read_row (index_table, NULL, &data_iov)))
+        {
+          if (!ret)
+            ca_set_error ("ca_table_read_row unexpectedly returned 0");
+
+          goto done;
+        }
+
+      data = data_iov.iov_base;
+
+      if (-1 == ca_data_parse_offset_score (&data, &token_offsets, &token_offset_count))
+        goto done;
+
+      if (invert_rank)
+        {
+          for (i = 0; i < token_offset_count; ++i)
+            token_offsets[i].score ^= 0xffffffff;
+        }
+
+      if (!offsets)
+        {
+          offsets = token_offsets;
+          offset_count = token_offset_count;
+        }
+      else
+        {
+          struct ca_offset_score *merged_offsets;
+          size_t merged_offset_count;
+          size_t max_merged_size;
+
+          if (subtract)
+            max_merged_size = offset_count;
+          else
+            max_merged_size = (token_offset_count > offset_count) ? offset_count : token_offset_count;
+
+          if (!(merged_offsets = safe_malloc (sizeof (*merged_offsets) * max_merged_size)))
+            goto done;
+
+          if (subtract)
+            {
+              merged_offset_count = CA_subtract_offsets (merged_offsets,
+                                                         offsets, offset_count,
+                                                         token_offsets, token_offset_count);
+            }
+          else
+            {
+              merged_offset_count = CA_intersect_offsets (merged_offsets,
+                                                          offsets, offset_count,
+                                                          token_offsets, token_offset_count);
+            }
+
+          free (offsets);
+          free (token_offsets);
+
+          offsets = merged_offsets;
+          offset_count = merged_offset_count;
+        }
     }
-
-  if (1 != (ret = ca_table_read_row (index_table, NULL, &data_iov)))
-    {
-      if (!ret)
-        ca_set_error ("ca_table_read_row unexpectedly returned 0");
-
-      goto done;
-    }
-
-  data = data_iov.iov_base;
-
-  if (-1 == ca_data_parse_offset_score (&data, &offsets, &offset_count))
-    goto done;
 
   if (offset_count < limit)
     limit = offset_count;
 
   ca_quicksort (offsets, offset_count);
+
+  /* XXX: Fetch documents in phsyical order */
 
   putchar ('[');
 
@@ -176,6 +312,7 @@ ca_schema_query (struct ca_schema *schema, const char *query,
 done:
 
   free (offsets);
+  free (query_buf);
 
   return result;
 }

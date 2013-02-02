e214cae23a8d09c044b0014593ae909f10917344
Morten Hustveit <morten.hustveit@gmail.com>
2013-02-02 13:34:30 -0800

    Added the "flex" table backend, with MVCC support
    Thanks to MVCC, this backend supports DELETE operations.  Combined, with
    INSERT, this allows UPDATE to be implemented as well, although at a higher
    abstraction level.
    
    Furthermore, this is the first backend that cannot be concurrently written to
    by multiple processes.  For example, inserting into the tables performs an
    lseek followed by a write.  The plan is to use explicit locking to prevent
    corruption.
    
    Signed-off-by: Morten Hustveit <morten.hustveit@gmail.com>

--- table.c
+++ table.c
@@ -7,6 +7,7 @@ uint64_t ca_xid;
 
 /*****************************************************************************/
 
+extern struct ca_table_backend CA_table_flexi;
 extern struct ca_table_backend CA_table_log;
 extern struct ca_table_backend CA_table_writeonce;
 
@@ -21,6 +22,9 @@ struct ca_table
 struct ca_table_backend *
 ca_table_backend (const char *name)
 {
+  if (!strcmp (name, "flexi"))
+    return &CA_table_flexi;
+
   if (!strcmp (name, "log"))
     return &CA_table_log;
 

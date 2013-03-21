a8c1adaa42955d1ffe1b7a21bc5aea405f6aed59
Morten Hustveit <morten.hustveit@gmail.com>
2013-03-21 12:53:01 -0700

    Added run-length encoding functions
    These will be used to better compress uint/float tuples.
    
    Signed-off-by: Morten Hustveit <morten.hustveit@gmail.com>

--- ca-internal.h
+++ ca-internal.h
@@ -1,9 +1,32 @@
 #ifndef CA_INTERNAL_H_
 #define CA_INTERNAL_H_ 1
 
+#include <stdlib.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+struct CA_rle_context
+{
+  uint8_t *data;
+  uint8_t value, run;
+};
+
+void
+CA_rle_init_write (struct CA_rle_context *ctx, uint8_t *output);
+
+void
+CA_rle_init_read (struct CA_rle_context *ctx, const uint8_t *input);
+
+uint8_t *
+CA_rle_flush (struct CA_rle_context *ctx);
+
+void
+CA_rle_put (struct CA_rle_context *ctx, uint8_t value);
+
+uint8_t
+CA_rle_get (struct CA_rle_context *ctx);
 
 struct CA_float_rank
 {

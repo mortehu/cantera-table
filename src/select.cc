defa5894524150a194833376127e9cebd6db9bdd
Aleksey Demakov <ademakov@gmail.com>
2017-10-27 05:48:34 +0700

    Reformat select.cc

--- src/select.cc
+++ src/select.cc
@@ -10,7 +10,7 @@
 #include "third_party/evenk/evenk/thread_pool.h"
 
 #if !HAVE_FPUTS_UNLOCKED
-# define fputs_unlocked fputs
+#define fputs_unlocked fputs
 #endif
 
 template <typename T>
@@ -21,50 +21,49 @@ namespace table {
 
 std::size_t CountFields(const select_statement& select) {
   std::size_t count = 0;
-  for (auto field = select.fields; field; field = field->next)
-    count++; 
+  for (auto field = select.fields; field; field = field->next) count++;
   return count;
 }
 
-void GetFieldValues(std::vector<std::vector<float>> &values, std::size_t field,
-		    struct Query* query, Schema* schema,
-		    const std::vector<ca_offset_score> &selection) try {
-    std::vector<ca_offset_score> field_offsets;
+void GetFieldValues(std::vector<std::vector<float>>& values, std::size_t field,
+                    struct Query* query, Schema* schema,
+                    const std::vector<ca_offset_score>& selection) try {
+  std::vector<ca_offset_score> field_offsets;
 
-    // TODO: Get rid of this
-    static std::mutex mutex;
-    {
-      std::unique_lock<std::mutex> lock(mutex);
-      ProcessQuery(field_offsets, query, schema, false, false);
+  // TODO: Get rid of this
+  static std::mutex mutex;
+  {
+    std::unique_lock<std::mutex> lock(mutex);
+    ProcessQuery(field_offsets, query, schema, false, false);
+  }
+
+  std::sort(field_offsets.begin(), field_offsets.end(),
+            [](const auto& lhs, const auto& rhs) {
+              if (lhs.offset != rhs.offset) return lhs.offset < rhs.offset;
+              return lhs.score < rhs.score;
+            });
+
+  bool all_zero = true;
+  for (const auto& v : field_offsets) {
+    if (v.score) {
+      all_zero = false;
+      break;
     }
+  }
 
-    std::sort(field_offsets.begin(), field_offsets.end(),
-              [](const auto& lhs, const auto& rhs) {
-      if (lhs.offset != rhs.offset) return lhs.offset < rhs.offset;
-      return lhs.score < rhs.score;
-    });
+  auto vi = field_offsets.begin();
 
-    bool all_zero = true;
-    for (const auto& v : field_offsets) {
-      if (v.score) {
-        all_zero = false;
-        break;
-      }
-    }
+  for (size_t i = 0; i < selection.size(); ++i) {
+    const auto offset = selection[i].offset;
 
-    auto vi = field_offsets.begin();
+    while (vi != field_offsets.end() && vi->offset < offset) ++vi;
 
-    for (size_t i = 0; i < selection.size(); ++i) {
-      const auto offset = selection[i].offset;
-
-      while (vi != field_offsets.end() && vi->offset < offset) ++vi;
-
-      if (vi == field_offsets.end() || vi->offset > offset)
-        values[i][field] = std::numeric_limits<float>::quiet_NaN();
-      else
-        values[i][field] = all_zero ? 1.0f : vi->score;
-    }
-} catch (std::exception &x) {
+    if (vi == field_offsets.end() || vi->offset > offset)
+      values[i][field] = std::numeric_limits<float>::quiet_NaN();
+    else
+      values[i][field] = all_zero ? 1.0f : vi->score;
+  }
+} catch (std::exception& x) {
   printf("caught an exception: %s\n", x.what());
 } catch (...) {
   printf("caught unknown exception\n");
@@ -82,8 +81,7 @@ void Select(Schema* schema, const select_statement& select) {
   std::vector<std::vector<float>> values;
   auto n_fields = CountFields(select);
   values.resize(selection.size());
-  for (std::size_t i = 0; i < values.size(); ++i)
-    values[i].resize(n_fields);
+  for (std::size_t i = 0; i < values.size(); ++i) values[i].resize(n_fields);
 
   if (n_fields < 8) {
     std::size_t index = 0;
@@ -94,9 +92,9 @@ void Select(Schema* schema, const select_statement& select) {
 
     std::size_t index = 0;
     for (auto field = select.fields; field; field = field->next, ++index) {
-     	    thread_pool.submit([&values, index, field, schema, &selection] {
-			    GetFieldValues(values, index, field->query, schema, selection);
-			    });
+      thread_pool.submit([&values, index, field, schema, &selection] {
+        GetFieldValues(values, index, field->query, schema, selection);
+      });
     }
 
     thread_pool.wait();
@@ -116,8 +114,7 @@ void Select(Schema* schema, const select_statement& select) {
         SEEK_SET);
 
     string_view key, data;
-    KJ_REQUIRE(
-        summary_tables[summary_table_idx].second->ReadRow(key, data));
+    KJ_REQUIRE(summary_tables[summary_table_idx].second->ReadRow(key, data));
 
     printf("%.*s", static_cast<int>(key.size()), key.data());
 
@@ -132,8 +129,7 @@ void Select(Schema* schema, const select_statement& select) {
     if (select.with_summaries) {
       printf(",\"");
       for (const auto ch : data) {
-        if (ch == '"')
-          putchar('"');
+        if (ch == '"') putchar('"');
         putchar(ch);
       }
       putchar('"');
